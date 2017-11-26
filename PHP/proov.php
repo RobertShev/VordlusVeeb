91da3eaa52b81434bd96feccf231acab619406ad
Tauri Taevik <31793402+ttaevik@users.noreply.github.com>
2017-11-26 20:16:36 +0200

    proovime

---
+++ PHP/proov.php
@@ -0,0 +1,39 @@
+
+<?php
+//$mysqli defineerimata
+
+	if(isset($_POST["submit"])){
+		
+		if(isset($_POST["nimetus"]) and isset($_POST["kirjeldus"])){
+			echo $_POST["nimetus"];
+			
+			$stmt = $mysqli->prepare("INSERT INTO komponent (nimetus, kirjeldus) VALUES (?, ?)");
+			$stmt->bind_param("ss", $_POST["nimetus"], $_POST["kirjeldus"]);
+			$stmt->execute();			
+		}
+		
+	}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+	<meta charset="utf-8">
+	<title>sisesta andmed</title>
+</head>
+<body>
+
+	<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+	<p> sisesta andmed: </p>
+		<label>Nimetus</label> 
+		<input type="text" name= "nimetus">
+		<br><br>
+		<label>kirjeldus</label> 
+		<input type="text" name= "kirjeldus">
+		<br><br>
+		<input name="submit" type="submit" value="Sisesta">
+		
+	</form>
