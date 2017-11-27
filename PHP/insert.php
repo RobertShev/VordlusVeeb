eddb6e65c9219e1beb8a6dadb1cdda0b23e04d82
Tauri Taevik <ttaevik@greeny.cs.tlu.ee>
2017-11-27 13:44:45 +0200

    insert.php ühendus toimib

---
+++ PHP/insert.php
@@ -0,0 +1,60 @@
+
+<?php
+
+
+require("functions.php");
+$database = "if17_ttaevik_2";
+
+// Create connection
+$mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
+
+
+	if(isset($_POST["submit"])){
+		
+		if(isset($_POST["pcname"]) and isset($_POST["pccpu"]) and isset($_POST["pcgpu"]) and isset($_POST["storage"])){
+			//echo $_POST["pcname"];
+			
+			$stmt = $mysqli->prepare("INSERT INTO computers (pcname, pccpu, pcgpu, storage,email) VALUES (?, ?, ?, ? ,?)");
+			echo $mysqli->error;
+			$stmt->bind_param("sssis", $_POST["pcname"], $_POST["pccpu"],$_POST["pcgpu"],$_POST["storage"], $_POST["email"]);
+			$stmt->execute();
+			echo $stmt->error;
+			echo $_POST["pcname"];		
+		}
+	
+	$stmt->close();
+	$mysqli->close();	
+	}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+	<meta charset="utf-8">
+	<title>sisesta andmed</title>
+</head>
+<body>
+
+	<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+	<p> Palun sisesta andmed: </p>
+		<label>Nimetus</label> 
+		<input type="text" name= "pcname">
+		<br><br>
+		<label>Protsessor</label> 
+		<input type="text" name= "pccpu">
+		<br><br>
+		<label>Graafikakaart</label> 
+		<input type="text" name= "pcgpu">
+		<br><br>
+		<label>Kõvaketas</label> 
+		<input type="text" name= "storage">
+		<br><br>
+		<label>Email</label> 
+		<input type="text" name= "email">
+		<br><br>
+		<input name="submit" type="submit" value="Sisesta">
+		
+	</form>
