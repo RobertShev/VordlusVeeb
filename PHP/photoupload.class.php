921186292d2a8d2357d874b957c92b8bde2a0b6b
Kert Tamm <tammkert@greeny.cs.tlu.ee>
2017-12-04 13:11:27 +0200

    lisatud photoupload

---
+++ PHP/photoupload.class.php
@@ -0,0 +1,128 @@
+<?php
+	class Photoupload {
+		//properties ehk muutujad, methods ehk funktsioonid
+		/*public $testPublic;
+		public $testPrivate;*/
+		//protected
+		private $tempName;
+		private $imageFileType;
+		private $myTempImage;
+		private $myImage;
+		public $exifToImage;
+		
+		function __construct($name, $type){
+			/*$this->testPublic = "Väga avalik muutuja!";
+			$this->testPrivate = $x;*/
+			$this->tempName = $name;
+			$this->imageFileType = $type;
+		}
+		
+		private function createImage(){
+			//lähtudes failitüübist, loome pildiobjekti
+			if($this->imageFileType == "jpg" or $this->imageFileType == "jpeg"){
+				$this->myTempImage = imagecreatefromjpeg($this->tempName);
+			}
+			if($this->imageFileType == "png"){
+				$this->myTempImage = imagecreatefrompng($this->tempName);
+			}
+			if($this->imageFileType == "gif"){
+				$this->myTempImage = imagecreatefromgif($this->tempName);
+			}
+		}
+		
+		public function resizeImage($width, $height){
+			$this->createImage();
+			$imageWidth = imagesx($this->myTempImage);
+			$imageHeight = imagesy($this->myTempImage);
+			
+			$sizeRatio = 1;
+			if($imageWidth > $imageHeight){
+				$sizeRatio = $imageWidth / $width;
+			} else {
+				$sizeRatio = $imageHeight / $height;
+			}
+			$this->myImage = $this->resize_image($this->myTempImage, $imageWidth, $imageHeight, round($imageWidth / $sizeRatio), round($imageHeight / $sizeRatio));
+		}
+		
+		// private function resize_image($image, $origW, $origH, $w, $h){
+			// $dst = imagecreatetruecolor($w, $h);
+			// //säilitan png jaoks läbipaistvuse
+			// imagesavealpha($dst, true);
+			// $transColor = imagecolorallocatealpha($dst, 0, 0, 0, 127);
+			// imagefill($dst, 0, 0, $transColor);
+			// imagecopyresampled($dst, $image, 0, 0, 0, 0, $w, $h, $origW, $origH);
+			// return $dst;
+		// }
+		
+		// public function addWatermark($marginHor, $marginVer){
+			// //lisame vesimärgi
+			// $stamp = imagecreatefrompng("../graphics/hmv_logo.png");
+			// $stampWidth = imagesx($stamp);
+			// $stampHeight = imagesy($stamp);
+			// $stampPosX = imagesx($this->myImage) - $stampWidth - $marginHor;
+			// $stampPosY = imagesy($this->myImage) - $stampHeight - $marginVer;
+			// imagecopy($this->myImage, $stamp, $stampPosX, $stampPosY, 0, 0, $stampWidth, $stampHeight);
+		// }
+		
+		public function readExif(){
+			//loen EXIF infot
+			@$exif = exif_read_data($this->tempName, "ANY_TAG", 0, true);
+			//var_dump($exif);
+			if(!empty($exif["DateTimeOriginal"])){
+				$this->exifToImage = "Pilt tehti: " .$exif["DateTimeOriginal"];
+			} else {
+				$this->exifToImage = "Pildistamise aeg teadmata!";
+			}
+		}
+		
+		// public function addTextWatermark($text){
+			// //värv
+			// //imagecolorallocate - ilma läbipaistvuseta
+			// //alpha 0 - 127
+			// $textColor = imagecolorallocatealpha($this->myImage, 255, 255, 255, 50);
+			// imagettftext($this->myImage, 20, 0, 10, 25, $textColor, "../graphics/FREESCPT.TTF", $text);
+		// }
+		
+		public function savePhoto($directory, $fileName){
+			//salvestame pildifaili
+			$target_file = $directory .$fileName;
+			if($this->imageFileType == "jpg" or $this->imageFileType == "jpeg"){
+				if(imagejpeg($this->myImage, $target_file, 90)){
+					$notice = "true";
+				} else {
+					$notice = "false";
+				}
+			}
+			if($this->imageFileType == "png"){
+				if(imagepng($this->myImage, $target_file, 6)){
+					$notice = "true";
+				} else {
+					$notice = "false";
+				}
+			}
+			if($this->imageFileType == "gif"){
+				if(imagegif($this->myImage, $target_file)){
+					$notice = "true";
+				} else {
+					$notice = "false";
+				}
+			}
+			return $notice;
+		}
+		
+		public function saveOriginal($directory, $fileName){
+			$target_file = $directory .$fileName;
+			if (move_uploaded_file($this->tempName, $target_file)) {
+				$notice .= "true";
+			} else {
+				$notice .= "false";
+			}
+		}
+		
+		public function clearImages(){
+			imagedestroy($this->myTempImage);
+			imagedestroy($this->myImage);
+		}
+		
+	} // class'i lõpp
+?>
---
+++ PHP/photoupload.php
@@ -0,0 +1,95 @@
+<?php
+	require("functions.php");
+	require("photoupload.class.php");
+	$notice = "";
+	
+	//kui pole sisseloginud, siis sisselogimise lehele
+	if(!isset($_SESSION["userId"])){
+		header("Location: main.php");
+		exit();
+	}
+	
+	//kui logib välja
+	if (isset($_GET["logout"])){
+		//lõpetame sessiooni
+		session_destroy();
+		header("Location: main.php");
+	}
+	
+	//foto laadimise osa
+	$target_dir = "../pics/";
+	$target_file = "";
+	$uploadOk = 1;
+	$imageFileType = "";
+	$maxWidth = 600;
+	$maxHeight = 400;
+	$marginVer = 10;
+	$marginHor = 10;
+	
+	//Kas vajutati üleslaadimise nuppu
+	if(isset($_POST["submit"])) {
+		
+		if(!empty($_FILES["fileToUpload"]["name"])){
+			
+			//$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+			$imageFileType = strtolower(pathinfo(basename($_FILES["fileToUpload"]["name"]))["extension"]);
+			$timeStamp = microtime(1) *10000;
+			//$target_file = $target_dir . pathinfo(basename($_FILES["fileToUpload"]["name"]))["filename"] ."_" .$timeStamp ."." .$imageFileType;
+			$target_file = "hmv_" .$timeStamp ."." .$imageFileType;
+		
+			$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+			if($check !== false) {
+				$notice .= "Fail on pilt - " . $check["mime"] . ". ";
+				$uploadOk = 1;
+			} else {
+				$notice .= "See pole pildifail. ";
+				$uploadOk = 0;
+			}
+		
+			//Kas selline pilt on juba üles laetud
+			if (file_exists($target_file)) {
+				$notice .= "Kahjuks on selle nimega pilt juba olemas. ";
+				$uploadOk = 0;
+			}
+			//Piirame faili suuruse
+			/*if ($_FILES["fileToUpload"]["size"] > 1000000) {
+				$notice .= "Pilt on liiga suur! ";
+				$uploadOk = 0;
+			}*/
+			
+			//Piirame failitüüpe
+			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+				$notice .= "Vabandust, vaid jpg, jpeg, png ja gif failid on lubatud! ";
+				$uploadOk = 0;
+			}
+			
+			//Kas saab laadida?
+			if ($uploadOk == 0) {
+				$notice .= "Vabandust, pilti ei laetud üles! ";
+			//Kui saab üles laadida
+			} else {		
+								
+				//kasutame klassi
+				$myPhoto = new Photoupload($_FILES["fileToUpload"]["tmp_name"], $imageFileType);
+				$myPhoto->readExif();
+				$myPhoto->resizeImage($maxWidth, $maxHeight);
+				$myPhoto->addWatermark($marginHor, $marginVer);
+				//$myPhoto->addTextWatermark($myPhoto->exifToImage);
+				$myPhoto->addTextWatermark("Heade mõtete veeb");
+				$notice = $myPhoto->savePhoto($target_dir, $target_file);
+				if($notice == "true"){
+					$notice = "Pilt laeti üles!";
+				} else {
+					$notice = "Pildi üleslaadimine ebaõnnestus!";
+				}
+				//$myPhoto->saveOriginal(kataloog, failinimi);
+				$myPhoto->clearImages();
+				unset($myPhoto);
+				
+			}
+		
+		} else {
+			$notice = "Palun valige kõigepealt pildifail!";
+		}//kas faili nimi on olemas lõppeb
+	}//kas üles laadida lõppeb
+?>
