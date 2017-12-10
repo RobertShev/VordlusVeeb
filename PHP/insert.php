0d0b9bf058be91dc03d829d7e4a8818c7d81f239
Robert Shevchenko <robertshev@users.noreply.giyhub.com>
2017-12-10 17:54:10 +0200

    Andmete sisestamine

--- PHP/insert.php
+++ PHP/insert.php
@@ -9,48 +9,48 @@ $database = "if17_ttaevik_2";
 		session_destroy(); //lõpetab sessiooni
 		header("Location: main2.php");
 	}
-	
+
 // Create connection
 $mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
 
 
 	if(isset($_POST["submit"])){
-		
+
 		if(isset($_POST["pcname"]) and isset($_POST["pccpu"]) and isset($_POST["pcgpu"]) and isset($_POST["storage"])){
 			//echo $_POST["pcname"];
-			
+
 			$stmt = $mysqli->prepare("INSERT INTO computers (pcname, pccpu, pcgpu, storage,email) VALUES (?, ?, ?, ? ,?)");
 			echo $mysqli->error;
 			$stmt->bind_param("sssis", $_POST["pcname"], $_POST["pccpu"],$_POST["pcgpu"],$_POST["storage"], $_POST["email"]);
 			$stmt->execute();
 			echo $stmt->error;
-			echo $_POST["pcname"];		
+			echo $_POST["pcname"];
 		}
-	
+
 	$stmt->close();
-	$mysqli->close();	
+	$mysqli->close();
 	}
 
 	//FOTOUPLOADˇ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ˇ
-	
+
 
 	require("photoupload.class.php");
 	$notice = "";
-	
-	
+
+
 	//kui pole sisseloginud, siis sisselogimise lehele
 	if(!isset($_SESSION["userId"])){
 		header("Location: main2.php");
 		exit();
 	}
-	
+
 	//kui logib välja
 	if (isset($_GET["logout"])){
 		//lõpetame sessiooni
 		session_destroy();
 		header("Location: main2.php");
 	}
-	
+
 	//Algab foto laadimise osa
 	$target_dir = "../pics/";
 	$target_file = "";
@@ -60,18 +60,18 @@ $mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
 	$maxHeight = 400;
 	$marginVer = 10;
 	$marginHor = 10;
-	
+
 	//Kas vajutati üleslaadimise nuppu
 	if(isset($_POST["uploadsubmit"])) {
-		
+
 		if(!empty($_FILES["fileToUpload"]["name"])){
-			
+
 			//$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 			$imageFileType = strtolower(pathinfo(basename($_FILES["fileToUpload"]["name"]))["extension"]);
 			$timeStamp = microtime(1) *10000;
 			//$target_file = $target_dir . pathinfo(basename($_FILES["fileToUpload"]["name"]))["filename"] ."_" .$timeStamp ."." .$imageFileType;
 			$target_file = "pic_" .$timeStamp ."." .$imageFileType;
-		
+
 			$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
 			if($check !== false) {
 				$notice .= "Fail on pilt - " . $check["mime"] . ". ";
@@ -80,7 +80,7 @@ $mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
 				$notice .= "See pole pildifail. ";
 				$uploadOk = 0;
 			}
-		
+
 			//Kas selline pilt on juba üles laetud
 			if (file_exists($target_file)) {
 				$notice .= "Kahjuks on selle nimega pilt juba olemas. ";
@@ -91,19 +91,19 @@ $mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
 				$notice .= "Pilt on liiga suur! ";
 				$uploadOk = 0;
 			}*/
-			
+
 			//Piirame failitüüpe
 			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
 				$notice .= "Vabandust, vaid jpg, jpeg, png ja gif failid on lubatud! ";
 				$uploadOk = 0;
 			}
-			
+
 			//Kas saab laadida?
 			if ($uploadOk == 0) {
 				$notice .= "Vabandust, pilti ei laetud üles! ";
 			//Kui saab üles laadida
-			} else {		
-								
+			} else {
+
 				//kasutame klassi
 				$myPhoto = new Photoupload($_FILES["fileToUpload"]["tmp_name"], $imageFileType);
 				$myPhoto->readExif();
@@ -120,9 +120,9 @@ $mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
 				//$myPhoto->saveOriginal(kataloog, failinimi);
 				$myPhoto->clearImages();
 				unset($myPhoto);
-				
+
 			}
-		
+
 		} else {
 			$notice = "Palun valige kõigepealt pildifail!";
 		}//kas faili nimi on olemas lõppeb
@@ -136,47 +136,50 @@ $mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
 <head>
 	<meta charset="utf-8">
 	<title>Kuulutuse lisamine</title>
+	<link href="css/style.css" rel='stylesheet' type='text/css'/>
+		<link href="//fonts.googleapis.com/css?family=Ropa+Sans:400,400i&amp;subset=latin-ext" rel="stylesheet">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="keywords" content="Simple Tab Forms Widget Responsive, Login Form Web Template, Flat Pricing Tables, Flat Drop-Downs, Sign-Up Web Templates, Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design" />
 </head>
 <body>
 
-
-	<p><a href="?logout=1">Logi välja</a></p>
-	<p><a href="changedata.php">Muuda kuulutust</a></p>
-	<p><a href="timeline2.php">Vaata kuulutusi</a></p>
-	<hr>
+	<div class="sap_tabs">
+		<div id="horizontalTab" style="display: block; width: 100%; margin: 0px;">
+			<ul>
+				<li class="resp-tab-item" aria-controls="tab_item-0" role="tab"><a href="?logout=1">Logi välja</a></li>
+				<li class="resp-tab-item" aria-controls="tab_item-1" role="tab"><a href="changedata.php">Muuda kuulutust</a></li>
+				<li class="resp-tab-item" aria-controls="tab_item-2" role="tab"><a href="timeline2.php">Vaata kuulutusi</a></li>
+				<div class="clear"></div>
+			</ul>
+		</div>
+	</div>
 	<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
 	<center>
-	<p><strong>Sisesta müüdava arvuti andmed: </strong></p>
-		<label>Nimetus</label> 
-		<input type="text" name= "pcname">
-		<br><br>
-		<label>Protsessor</label> 
-		<input type="text" name= "pccpu">
-		<br><br>
-		<label>Graafikakaart</label> 
-		<input type="text" name= "pcgpu">
-		<br><br>
-		<label>Kõvaketta maht(GB)</label> 
-		<input type="text" name= "storage">
-		<br><br>
-		<label>Email</label> 
-		<input type="text" name= "email">
-		<br><br>
-		<input name="submit" type="submit" value="Sisesta">
-		<br><br>
-		<br><br>
-		<label>Valige pildifail:</label>
-		<input type="file" name="fileToUpload" id="fileToUpload">
-		<input type="submit" value="Lae üles" name="uploadsubmit" id="photoSubmit"><span id="fileSizeError"></span>
-				
-		
+	<div class="main-content">
+		<h1>Sisesta müüdava arvuti andmed: </h1>
+			<input placeholder="Nimetus" name="pcname" type="text" required="">
+			<input placeholder="Protsessor" name="pccpu" type="text" required="">
+			<input placeholder="Graafikakaart" name="pcgpu" type="text" required="">
+			<input placeholder="Kõvaketta maht(GB)" name="storage" type="text" required="">
+			<input placeholder="Email" name="storage" type="text" required="">
+			<input name="submit" type="submit" value="Sisesta">
+			<br />
+			<div class="agile-tb">
+				<h1>Vali pilt</h1>
+				<br />
+				<input  name="fileToUpload" name="pcname" type="file" id="fileToUpload" required="">
+				<input type="submit" value="Lae üles" name="uploadsubmit" id="photoSubmit"><span id="fileSizeError"></span>
+			</div>
+	</div>
 
-	
+
+
 <span><?php echo $notice; ?></span>
-		
-		
-	</center>	
+
+
+	</center>
 	</form>
-	
-	
+
+
 	</body>
